1e20d39c8cb510ff45dce3a5e0cb85a8558e7b59
sturcios <sen.turcios@gmail.com>
2018-10-04 21:51:13 -0500

    Added code to Home page and made chages to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-import HomeNavbar from "./components/HomeNavbar";
-import SearchBar from "./components/SearchBar";
+import AppNavbar from "./components/AppNavbar";
+//import SearchBar from "./components/SearchBar";
 import FooterPage from "./components/FooterPage";
+import { Switch, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import ProfilePage from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import SearchResults from "./pages/SearchResults";
+
 
 
 
@@ -10,16 +17,23 @@ import FooterPage from "./components/FooterPage";
 class App extends Component {
   render() {
     return (
-         <div> 
-        <HomeNavbar />
-     
-          <SearchBar />
+      <div>
+        <AppNavbar />
 
-          <FooterPage />
-          </div>
-       
-       
-    
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/search' component={SearchResults} />
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/profilepage' component={ProfilePage} />
+        </Switch>
+
+        <FooterPage />
+      </div>
+
+
+
     );
   }
 }
