89582b4a4b3b3ef7e79459d6b8988aa35bb6ad20
Raquel Azcue <rvazcue@gmail.com>
2018-10-08 12:34:36 -0500

    the search  bar on the Home page now has a button; when pressed the button console logs the inputed text; now we just need to grab that info and redirect to a results page that displays the item queried

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,16 +1,62 @@
 import React, { Component } from 'react';
-import SearchBar from "../../components/SearchBar";
 import Header from "../../components/Header"
+import SearchBar from "../../components/SearchBar";
+import SearchButton from "../../components/SearchButton";
+
 
 
 
 class Home extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      results: [],
+      searchText: ""
+    }
+  }
+
+  handleInputChange = event => {
+    let value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({
+      [name]: value
+    })
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (!this.state.searchText) {
+      alert("must enter search word");
+    }
+    else {
+      let tempResults = [];
+      
+      let options = {
+        keys: ['item']
+      };
+      
+
+      console.log(this.state);
+
+    }
+  };
+
+
   render() {
     return (
       <div>
         <Header />
 
-        <SearchBar />
+
+
+        <div className="input-group" id="searchHolder">
+          <SearchBar className="form-control" onChange={this.handleInputChange} value={this.state.searchText} />
+          <span>
+            <SearchButton onClick={this.handleSubmit} />
+          </span>
+          </div>
 
 
       </div>
