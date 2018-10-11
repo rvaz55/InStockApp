c143ce56b653349ba86d9111278905e762ece2bd
Mel Sidd <mlsiddoway@gmail.com>
2018-10-11 12:27:02 -0500

    removed redux packages and code for it and added axios requests in the utils folder to get search results to showup on searchresults page

--- client/src/utilsClient/routesClient.js
+++ client/src/utilsClient/routesClient.js
@@ -8,9 +8,13 @@ export default {
   getAllItems: function() {
     return axios.get("/api/items/")
   },
-  // Gets the item with the given id
-  getItem: function(id) {
-    return axios.get("/api/items/" + id);
+  // Gets the item with the given search term
+  getItemsBySearch: function(search) {
+    return axios.get(`/api/items/search/${search}`);
+  },
+  // Gets the item with the given category selection
+  getItemsByCategory: function(category) {
+    return axios.get(`/api/items/category/${category}`);
   },
   // Deletes the item with the given id
   deleteItem: function(id) {
